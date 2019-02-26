2063a02ebeaefb7e92fcc4cfc7bc7ab537ae0fdc
denniedeng <289447554@qq.com>
2019-02-26 17:46:27 +0800

    test for jenkins to build auto

--- src/main/java/com/xx/CloudInitMainApplication.java
+++ src/main/java/com/xx/CloudInitMainApplication.java
@@ -27,6 +27,7 @@ public class CloudInitMainApplication
 {
     public static void main(String[] args)
     {
+    	System.out.println("test commit");
         SpringApplication.run(CloudInitMainApplication.class, args);
     }
 
